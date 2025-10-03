edad2ebffbaa7199d2e9d7cb719d9b1b871dff54
GiacomoBorsellino <giacomo.borsellino@avrgroup.it>
2025-10-03 10:12:39 +0200

    link

--- FE/src/Sidebar.tsx
+++ FE/src/Sidebar.tsx
@@ -11,6 +11,12 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Link } from "react-router-dom";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./components/ui/tooltip";
 
 export default function Sidebar() {
   return (
@@ -30,7 +36,7 @@ export default function Sidebar() {
           <ul className="flex flex-col justify-start gap-2">
             <Link to={"/list"}>
               <li className="text-xl font-semibold border-2 border-black rounded-md bg-white p-2 hover:bg-amber-400 cursor-pointer">
-                - Courses
+                - Notes
               </li>
             </Link>
             <Link to={"/wtf"}>
@@ -46,21 +52,64 @@ export default function Sidebar() {
           </ul>
 
           <div className="flex flex-row justify-center items-center gap-2">
-            <Button className="bg-green-400 w-10 h-10">
-              <span className="material-symbols text-2xl font-bold cursor-pointer">
-                web
-              </span>
-            </Button>
-            <Button className="bg-blue-400 w-10 h-10">
-              <span className="material-symbols text-2xl font-bold cursor-pointer">
-                work
-              </span>
-            </Button>
-            <Button className="bg-purple-400 w-10 h-10">
-              <span className="material-symbols text-2xl font-bold cursor-pointer">
-                info
-              </span>
-            </Button>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <a
+                    href="https://github.com/GiacomoBorsellino"
+                    target="_blank"
+                  >
+                    <Button className="bg-green-400 w-10 h-10">
+                      <span className="material-symbols text-2xl font-bold cursor-pointer">
+                        web
+                      </span>
+                    </Button>
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>GitHub</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <a
+                    href="https://github.com/GiacomoBorsellino/app-unti2"
+                    target="_blank"
+                  >
+                    <Button className="bg-blue-400 w-10 h-10">
+                      <span className="material-symbols text-2xl font-bold cursor-pointer">
+                        change_history
+                      </span>
+                    </Button>
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Code</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <a
+                    href="https://giacomoborsellino.netlify.app/"
+                    target="_blank"
+                  >
+                    <Button className="bg-purple-400 w-10 h-10">
+                      <span className="material-symbols text-2xl font-bold cursor-pointer">
+                        person
+                      </span>
+                    </Button>
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>My website</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </div>
           <SheetFooter>
             <SheetClose asChild>
---
+++ FE/todo.md
@@ -0,0 +1,17 @@
+## Todo
+
+# FE
+
+- Scrivi wtf
+- Amplia about me
+- Metti input ricerca in list
+- togli bottoniera
+
+# BE
+
+- semplice db
+- blob thumb
+- user
+- definisci er
+- graphql upload
+- refresh token
